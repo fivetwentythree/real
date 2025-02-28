2914c22b6eb6037e9eeb70008d7c3972d0f191eb
Lochana Perera <lochana-mbp@Lochanas-MacBook-Pro.local>
2025-02-28 22:44:17 +1100

    made the menu working

--- src/site.config.ts
+++ src/site.config.ts
@@ -21,19 +21,19 @@ export const siteConfig: SiteConfig = {
 // Used to generate links in both the Header & Footer.
 export const menuLinks: { path: string; title: string }[] = [
 	{
-		path: "/",
+		path: "/real/",
 		title: "Home",
 	},
 	{
-		path: "/about/",
+		path: "/real/about/",
 		title: "About",
 	},
 	{
-		path: "/posts/",
+		path: "/real/posts/",
 		title: "Blog",
 	},
 	{
-		path: "/notes/",
+		path: "/real/notes/",
 		title: "Notes",
 	},
 ];
